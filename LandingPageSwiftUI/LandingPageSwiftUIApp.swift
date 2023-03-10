cdba8c5746b1ef1a8284f9b79adc68e0c56e54d8
Oliver <oliverrc83@me.com>
2023-03-10 15:09:11 +0100

    First Commit

---
+++ LandingPageSwiftUI/LandingPageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandingPageSwiftUIApp.swift
+//  LandingPageSwiftUI
+//
+//  Created by MacAmd on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LandingPageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
